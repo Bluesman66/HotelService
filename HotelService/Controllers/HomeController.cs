709ec68fd545e10e8b2cdabf870c03b7269913e2
alex.paclin <alex.paclin@gmail.com>
2018-04-21 22:54:44 +0500

    HotelService3

--- HotelService/Controllers/HomeController.cs
+++ HotelService/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace HotelService.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ActionResult Index()
         {
-            return "Сайт для гостиницы";
+            return View();
         }
     }
 }
