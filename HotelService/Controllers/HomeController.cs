5c90749740c4e1b6fe20334f9506fab20b6cd5ed
alex.paclin <alex.paclin@gmail.com>
2018-04-21 23:16:43 +0500

    HotelService5

--- HotelService/Controllers/HomeController.cs
+++ HotelService/Controllers/HomeController.cs
@@ -10,9 +10,11 @@ namespace HotelService.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.VisitorNumber = new Random().Next(1, 100);
-            ViewBag.DateAndTime = DateTime.Now;
-            ViewData["VisitorNumber"] = new Random().Next(1, 100);
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
             return View();
         }
     }
